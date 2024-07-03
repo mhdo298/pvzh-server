fcb02eb1f266b786b3407e93923974ae304e48c7
mhdo <mhdo@>
2024-07-03 02:58:15 -0400

    redirected

--- src/assetbundles.py
+++ src/assetbundles.py
@@ -9,9 +9,18 @@ assetbundles = Blueprint('assetbundles', __name__, url_prefix='/assetbundles')
 
 @assetbundles.route('/<path:path>', methods=['GET'])
 def bundles(path: str):
+    if path.endswith('/cards/card_data'):
+        return redirect('https://github.com/PrplGreen2/Syndrome/releases/download/v4.1.0.2.2/card_data_173')
+    if path.endswith('/data_assets'):
+        return redirect('https://github.com/PrplGreen2/Syndrome/releases/download/v4.1.0.2.2/data_assets_360')
+    if path.endswith('/fonts/inline_text_tag'):
+        return redirect('https://github.com/PrplGreen2/Syndrome/releases/download/v4.1.0.2.2/inline_text_tag_40')
+    if path.endswith('/loc/en'):
+        return redirect('https://github.com/PrplGreen2/Syndrome/releases/download/v4.1.0.2.2/en_296')
+
     if path.endswith('/manifest_version'):
         version = r.incr('manifest')
-        return str(version)
+        return version
     if path.endswith('/AssetPathsManifest'):
         version = r.get('manifest')
         content = (assets
