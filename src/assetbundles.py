1adc0e3d54f7951152dcb1e05caeeebbed159cd8
mhdo <mhdo@>
2024-07-03 03:37:05 -0400

    clouded

--- src/assetbundles.py
+++ src/assetbundles.py
@@ -5,32 +5,29 @@ from utils import r, assets
 from gzip import compress
 
 assetbundles = Blueprint('assetbundles', __name__, url_prefix='/assetbundles')
+mod_files = ['/cards/card_data', '/data_assets', '/fonts/inline_text_tag', '/loc/en']
 
 
 @assetbundles.route('/<path:path>', methods=['GET'])
 def bundles(path: str):
-    if path.endswith('/cards/card_data'):
-        return redirect('https://github.com/PrplGreen2/Syndrome/releases/download/v4.1.0.2.2/card_data_173')
-    if path.endswith('/data_assets'):
-        return redirect('https://github.com/PrplGreen2/Syndrome/releases/download/v4.1.0.2.2/data_assets_360')
-    if path.endswith('/fonts/inline_text_tag'):
-        return redirect('https://github.com/PrplGreen2/Syndrome/releases/download/v4.1.0.2.2/inline_text_tag_40')
-    if path.endswith('/loc/en'):
-        return redirect('https://github.com/PrplGreen2/Syndrome/releases/download/v4.1.0.2.2/en_296')
-
+    for mod_file in mod_files:
+        if mod_file in path:
+            return redirect('https://storage.googleapis.com/devh-mod-files/files/' + path)
     if path.endswith('/manifest_version'):
         version = r.incr('manifest')
         return str(version)
     if path.endswith('/AssetPathsManifest'):
         version = r.get('manifest')
-        content = (assets
-                   .replace(b'"CARD_DATA_VERSION"', version)
-                   .replace(b'"DATA_ASSETS_VERSION"', version)
-                   .replace(b'"INLINE_TEXT_TAG_VERSION"', version)
-                   .replace(b'"EN_VERSION"', version)
-                   )
-        return content
+        content = gzip.compress(assets
+                                .replace(b'"CARD_DATA_VERSION"', version)
+                                .replace(b'"DATA_ASSETS_VERSION"', version)
+                                .replace(b'"INLINE_TEXT_TAG_VERSION"', version)
+                                .replace(b'"EN_VERSION"', version)
+                                )
+        response = make_response(content)
+        response.headers['Content-length'] = len(content)
+        response.headers['Content-Encoding'] = 'gzip'
+        return response
     if 'if-none-match' in request.headers.keys():
         return (None, 304)
-    # if path.endswith('/manifest.json'):
     return redirect('https://pvzheroes-live.ecs.popcap.com/assetbundles/' + path)
