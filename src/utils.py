65110007536ac4fa0d57771f2d9e57e7d1ffeea4
mhdo <mhdo@>
2024-07-05 06:12:26 -0400

    funky

--- src/utils.py
+++ src/utils.py
@@ -562,6 +562,11 @@ def make_entity_model(sd1, sd2):
                             "1": -1,
                             "2": int(x)
                         }] * plant_sd['Deck']['super'][x]]
+    max_hand = 15
+    max_health = 50
+    if '1005433873053' in [plant_id, zombie_id]:
+        max_hand = 10
+        max_health = 20
     model_json = {
         "1": [
             {
@@ -574,7 +579,7 @@ def make_entity_model(sd1, sd2):
                         "68": {
                             "2": {
                                 "1": {
-                                    "1": 50  # initial health?
+                                    "1": max_health  # initial health?
                                 },
                                 "2": 0
                             }
@@ -677,7 +682,7 @@ def make_entity_model(sd1, sd2):
             }, {
                 "1": [{
                     "20": {
-                        "1": 15  # max hand size?
+                        "1": max_hand  # max hand size?
                     }
                 }, {
                     "37": {
@@ -718,7 +723,7 @@ def make_entity_model(sd1, sd2):
                     "68": {
                         "2": {
                             "1": {
-                                "1": 50
+                                "1": max_health
                             },
                             "2": 0
                         }
@@ -822,7 +827,7 @@ def make_entity_model(sd1, sd2):
             {
                 "1": [{
                     "20": {
-                        "1": 15
+                        "1": max_hand
                     }
                 }, {
                     "37": {
